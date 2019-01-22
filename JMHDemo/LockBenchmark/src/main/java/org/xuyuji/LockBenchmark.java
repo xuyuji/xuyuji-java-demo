73f5d099dc81e89d102a65b8316becb16b214498
xuyuji <xuyuji1987@163.com>
2019-01-22 10:46:01 +0800

    add LockBenchmark

---
+++ JMHDemo/LockBenchmark/src/main/java/org/xuyuji/LockBenchmark.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.xuyuji;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@BenchmarkMode({Mode.SampleTime})
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations=3, time = 5, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations=1,batchSize = 100000000)
+@Threads(2)
+@Fork(1)
+@State(Scope.Benchmark)
+public class LockBenchmark {
+
+	Lock lock = new ReentrantLock();
+	long i = 0;
+	AtomicLong atomicLong = new AtomicLong(0);
+
+	@Benchmark
+	public void measureLock() {
+		lock.lock();
+		i++;
+		lock.unlock();
+	}
+
+	@Benchmark
+	public void measureCAS() {
+		atomicLong.incrementAndGet();
+	}
+
+	@Benchmark
+	public void measureNoLock() {
+		i++;
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options options = new OptionsBuilder()
+	            .include(LockBenchmark.class.getSimpleName())
+	            .mode(Mode.SampleTime)
+	            .output("Benchmark.log")
+	            .build();
+	    new Runner(options).run();
+	}
+}
